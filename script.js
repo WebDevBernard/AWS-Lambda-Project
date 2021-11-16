2072f01a6d76e3ce7a5c91579595570c7bf46433
Bernard Yang <ybernard@gmail.com>
2021-11-15 20:31:53 -0800

    lint and comments + date to console log in script file

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
 import fetch from "node-fetch";
+// API Documentation (Requests to this API are limited to 300 requests per minute): https://raider.io/api#/mythic_plus/getApiV1MythicplusAffixes
+// Example of API request: https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-1&dungeon=all&strict=false&affixes=tyrannical-inspiring-necrotic-prideful&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false
+// How this script works: https://www.reddit.com/r/wow/comments/o5nocw/comment/h2ov91n/?utm_source=share&utm_medium=web2x&context=3
+// Mythic+ Season 2 started on July 6th, 2021
 
-// Requests to this API are limited to 300 requests per minute
-// https://raider.io/api#/mythic_plus/getApiV1MythicplusAffixes
-// https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-1&dungeon=all&strict=false&affixes=tyrannical-inspiring-necrotic-prideful&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false
-// https://www.reddit.com/r/wow/comments/o5nocw/comment/h2ov91n/?utm_source=share&utm_medium=web2x&context=3
-
-// Season 2 starts July 6
-// 1	Fortified-Bursting-Storming-Tormented
-// 2	Tyrannical-Raging-Volcanic-Tormented
-// 3	Fortified-Inspiring-Grievous-Tormented
-// 4	Tyrannical-Spiteful-Necrotic-Tormented
-// 5	Fortified-Bolstering-Quaking-Tormented
-// 6	Tyrannical-Sanguine-Storming-Tormented
-// 7	Fortified-Raging-Explosive-Tormented
-// 8	Tyrannical-Bursting-Volcanic-Tormented
-// 9	Fortified-Spiteful-Grievous-Tormented
-// 10	Tyrannical-Inspiring	Quaking	Tormented
-// 11	Fortified-Sanguine-Necrotic-Tormented
-// 12	Tyrannical-Bolstering-Explosive-Tormented
-
+// change season with this variable
+const season = "2";
 // change schedule with this array
 const schedule = [
   "Fortified-Bursting-Storming",
@@ -34,57 +21,63 @@ const schedule = [
   "Fortified-Sanguine-Necrotic-Tormented",
   "Tyrannical-Bolstering-Explosive-Tormented",
 ];
+// change region with this variable. Available values : us, eu, tw, kr, cn
+const region = "world";
 
-const url1 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[0]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url2 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[1]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url3 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[2]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url4 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[3]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url5 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[4]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url6 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[5]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url7 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[6]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url8 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[7]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url9 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[8]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url10 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[9]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url11 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[10]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
-const url12 = `https://raider.io/api/mythic-plus/rankings/runs?region=world&season=season-sl-2&dungeon=all&strict=false&affixes=${schedule[11]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+// change this only if you need to modify a hardcoded parameter
+const week1 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[0]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week2 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[1]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week3 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[2]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week4 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[3]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week5 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[4]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week6 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[5]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week7 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[6]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week8 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[7]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week9 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[8]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week10 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[9]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week11 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[10]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
+const week12 = `https://raider.io/api/mythic-plus/rankings/runs?region=${region}&season=season-sl-${season}&dungeon=all&strict=false&affixes=${schedule[11]}&&page=0&limit=0&minMythicLevel=0&maxMythicLevel=0&eventId=0&faction=&realm=&period=0&recent=false`;
 
 const fetchData = async () => {
   try {
     const response = await Promise.all([
-      fetch(url1),
-      fetch(url2),
-      fetch(url3),
-      fetch(url4),
-      fetch(url5),
-      fetch(url6),
-      fetch(url7),
-      fetch(url8),
-      fetch(url9),
-      fetch(url10),
-      fetch(url11),
-      fetch(url12),
+      fetch(week1),
+      fetch(week2),
+      fetch(week3),
+      fetch(week4),
+      fetch(week5),
+      fetch(week6),
+      fetch(week7),
+      fetch(week8),
+      fetch(week9),
+      fetch(week10),
+      fetch(week11),
+      fetch(week12),
     ]);
     const jsonData = response.map((res) => res.json());
     const promiseData = await Promise.all(jsonData);
     return promiseData;
   } catch (err) {
-    console.log(err);
+    console.log("Could Not Return Request:", err);
   }
 };
 
 const organizeData = async () => {
   const data = await fetchData();
-  // console.log(data);
   let obj = {};
   for (let i = 0; i < data.length; i++) {
-    obj[`Week ${i + 1}`] = data[i].rankings.ui.lastPage * 20;
+    // There are 20 rankings per page, lastPage = number of pages. Therefore 20 * lastPage = total number of characters
+    obj[`Week${i + 1}`] = data[i].rankings.ui.lastPage * 20;
   }
   return obj;
 };
 
+const getDate = new Date();
+const readableDate = getDate.toISOString().slice(0, 10).replace(/-/g, "/");
+
 const printData = async () => {
-  const giveMeData = await organizeData();
-  console.log(giveMeData);
+  const data = await organizeData();
+  console.log(readableDate, ":", data);
 };
 
 printData();
