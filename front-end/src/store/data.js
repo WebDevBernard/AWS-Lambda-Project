87818c777c8323605e977765638e20b4f7015eac
Bernard Yang <ybernard@gmail.com>
2021-11-19 02:30:40 -0800

    refactor scheduleCalculate

---
+++ front-end/src/store/data.js
@@ -0,0 +1,176 @@
+const week1 = parseInt(filterAffix(1, 1));
+const week2 = parseInt(filterAffix(2, 2));
+const week3 = parseInt(filterAffix(3, 3));
+const week4 = parseInt(filterAffix(4, 4));
+const week5 = parseInt(filterAffix(5, 5));
+const week6 = parseInt(filterAffix(6, 6));
+const week7 = parseInt(filterAffix(7, 7));
+const week8 = parseInt(filterAffix(8, 8));
+const week9 = parseInt(filterAffix(9, 9));
+const week10 = parseInt(filterAffix(10, 10));
+const week11 = parseInt(filterAffix(11, 11));
+const week12 = parseInt(filterAffix(12, 12));
+const week13 = filterAffix(13, 1) - filterAffix(1, 1);
+const week14 = filterAffix(14, 2) - filterAffix(2, 2);
+const week15 = filterAffix(15, 3) - filterAffix(3, 3);
+const week16 = filterAffix(16, 4) - filterAffix(4, 4);
+const week17 = filterAffix(17, 5) - filterAffix(5, 5);
+const week18 = filterAffix(18, 6) - filterAffix(6, 6);
+const week19 = filterAffix(19, 7) - filterAffix(7, 7);
+const week20 = filterAffix(20, 8) - filterAffix(8, 8);
+const week21 = filterAffix(21, 9) - filterAffix(9, 9);
+const week22 = filterAffix(22, 10) - filterAffix(10, 10);
+const week23 = filterAffix(23, 11) - filterAffix(11, 11);
+const week24 = filterAffix(24, 12) - filterAffix(12, 12);
+const week25 = filterAffix(25, 1) - filterAffix(13, 1) - filterAffix(1, 1);
+const week26 = filterAffix(26, 2) - filterAffix(14, 2) - filterAffix(2, 2);
+const week27 = filterAffix(27, 3) - filterAffix(15, 3) - filterAffix(3, 3);
+const week28 = filterAffix(28, 4) - filterAffix(16, 4) - filterAffix(4, 4);
+const week29 = filterAffix(29, 5) - filterAffix(17, 5) - filterAffix(5, 5);
+const week30 = filterAffix(30, 6) - filterAffix(18, 6) - filterAffix(6, 6);
+const week31 = filterAffix(31, 7) - filterAffix(19, 7) - filterAffix(7, 7);
+const week32 = filterAffix(32, 8) - filterAffix(20, 8) - filterAffix(8, 8);
+const week33 = filterAffix(33, 9) - filterAffix(21, 9) - filterAffix(9, 9);
+const week34 = filterAffix(34, 10) - filterAffix(22, 10) - filterAffix(10, 10);
+const week35 = filterAffix(35, 11) - filterAffix(23, 11) - filterAffix(11, 11);
+const week36 = filterAffix(36, 12) - filterAffix(24, 12) - filterAffix(12, 12);
+const week37 =
+  filterAffix(37, 1) -
+  filterAffix(25, 1) -
+  filterAffix(13, 1) -
+  filterAffix(1, 1);
+const week38 =
+  filterAffix(38, 2) -
+  filterAffix(26, 2) -
+  filterAffix(14, 2) -
+  filterAffix(2, 2);
+const week39 =
+  filterAffix(39, 3) -
+  filterAffix(27, 3) -
+  filterAffix(15, 3) -
+  filterAffix(3, 3);
+const week40 =
+  filterAffix(40, 4) -
+  filterAffix(28, 4) -
+  filterAffix(16, 4) -
+  filterAffix(4, 4);
+const week41 =
+  filterAffix(41, 5) -
+  filterAffix(29, 5) -
+  filterAffix(17, 5) -
+  filterAffix(5, 5);
+const week42 =
+  filterAffix(42, 6) -
+  filterAffix(30, 6) -
+  filterAffix(18, 6) -
+  filterAffix(6, 6);
+const week43 =
+  filterAffix(43, 7) -
+  filterAffix(31, 7) -
+  filterAffix(19, 7) -
+  filterAffix(7, 7);
+const week44 =
+  filterAffix(44, 8) -
+  filterAffix(32, 8) -
+  filterAffix(20, 8) -
+  filterAffix(8, 8);
+const week45 =
+  filterAffix(45, 9) -
+  filterAffix(33, 9) -
+  filterAffix(21, 9) -
+  filterAffix(9, 9);
+const week46 =
+  filterAffix(46, 10) -
+  filterAffix(34, 10) -
+  filterAffix(22, 10) -
+  filterAffix(10, 10);
+const week47 =
+  filterAffix(47, 11) -
+  filterAffix(35, 11) -
+  filterAffix(23, 11) -
+  filterAffix(11, 11);
+const week48 =
+  filterAffix(48, 12) -
+  filterAffix(36, 12) -
+  filterAffix(24, 12) -
+  filterAffix(12, 12);
+const week49 =
+  filterAffix(49, 1) -
+  filterAffix(37, 1) -
+  filterAffix(25, 1) -
+  filterAffix(13, 1) -
+  filterAffix(1, 1);
+const week50 =
+  filterAffix(50, 2) -
+  filterAffix(38, 2) -
+  filterAffix(26, 2) -
+  filterAffix(14, 2) -
+  filterAffix(2, 2);
+const week51 =
+  filterAffix(51, 3) -
+  filterAffix(39, 3) -
+  filterAffix(27, 3) -
+  filterAffix(15, 3) -
+  filterAffix(3, 3);
+const week52 =
+  filterAffix(52, 4) -
+  filterAffix(40, 4) -
+  filterAffix(28, 4) -
+  filterAffix(16, 4) -
+  filterAffix(4, 4);
+
+const graphArray = [
+  week1,
+  week2,
+  week3,
+  week4,
+  week5,
+  week6,
+  week7,
+  week8,
+  week9,
+  week10,
+  week11,
+  week12,
+  week13,
+  week14,
+  week15,
+  week16,
+  week17,
+  week18,
+  week19,
+  week20,
+  week21,
+  week22,
+  week23,
+  week24,
+  week25,
+  week26,
+  week27,
+  week28,
+  week29,
+  week30,
+  week31,
+  week32,
+  week33,
+  week34,
+  week35,
+  week36,
+  week37,
+  week38,
+  week39,
+  week40,
+  week41,
+  week42,
+  week43,
+  week44,
+  week45,
+  week46,
+  week47,
+  week48,
+  week49,
+  week50,
+  week51,
+  week52,
+];
+const formatedArray = graphArray.filter((item) => item > 0);
