35505d7a88c0f9a7719cb8907ad069439f3f444e
Bernard Yang <ybernard@gmail.com>
2021-11-20 20:32:47 -0800

    add dev icon link

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -4,7 +4,9 @@ export default function Header({ season, expansionName, headerMessage }) {
   return (
     <div id="top" className={classes.header}>
       <h1 className={classes.title}>
-        <img src="https://img.icons8.com/cotton/64/000000/sun--v3.png" />
+        <a href="https://icons8.com/icon/OkWsH5jFVMve/sun">
+          <img src="https://img.icons8.com/cotton/64/000000/sun--v3.png" />
+        </a>
         <a href="https://www.bernardyang.com/" target="_blank" rel="noreferrer">
           {expansionName.toUpperCase()} M+ S{season} Player Count
         </a>
