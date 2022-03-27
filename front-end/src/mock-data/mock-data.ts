62dee4a0c5137249f29f4f7fbc4701231b6b853b
Bernard Yang <ybernard@gmail.com>
2022-03-26 21:40:56 -0700

    add new media query to button

--- front-end/src/mock-data/mock-data.ts
+++ front-end/src/mock-data/mock-data.ts
@@ -22,9 +22,10 @@ const firstTotal = Array.from({ length: 12 }, () =>
   mockNumber(700000, 1000000)
 );
 const secondTotal = Array.from({ length: 12 }, () =>
-  mockNumber(200000, 300000)
+  mockNumber(100000, 200000)
 );
 
+const thirdTotal = Array.from({ length: 12 }, () => mockNumber(10000, 20000));
 export const mockData = [
   {
     date: "2022-03-11T13:55:52PST",
@@ -219,5 +220,102 @@ export const mockData = [
     affix: schedule[11],
     expansion: "sl",
     total: firstTotal[11] + secondTotal[11],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 25,
+    season: 3,
+    affix: schedule[0],
+    expansion: "sl",
+    total: firstTotal[0] + secondTotal[0] + thirdTotal[0],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 26,
+    season: 3,
+    affix: schedule[1],
+    expansion: "sl",
+    total: firstTotal[1] + secondTotal[1] + thirdTotal[1],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 27,
+    season: 3,
+    affix: schedule[2],
+    expansion: "sl",
+    total: firstTotal[2] + secondTotal[2] + thirdTotal[2],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 28,
+    season: 3,
+    affix: schedule[3],
+    expansion: "sl",
+    total: firstTotal[3] + secondTotal[3] + thirdTotal[3],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 29,
+    season: 3,
+    affix: schedule[4],
+    expansion: "sl",
+    total: firstTotal[4] + secondTotal[4] + thirdTotal[4],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 30,
+    season: 3,
+    affix: schedule[5],
+    expansion: "sl",
+    total: firstTotal[5] + secondTotal[5] + thirdTotal[5],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 31,
+    season: 3,
+    affix: schedule[6],
+    expansion: "sl",
+    total: firstTotal[6] + secondTotal[6] + thirdTotal[6],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 32,
+    season: 3,
+    affix: schedule[7],
+    expansion: "sl",
+    total: firstTotal[7] + secondTotal[7] + thirdTotal[7],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 33,
+    season: 3,
+    affix: schedule[8],
+    expansion: "sl",
+    total: firstTotal[8] + secondTotal[8] + thirdTotal[8],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 34,
+    season: 3,
+    affix: schedule[9],
+    expansion: "sl",
+    total: firstTotal[9] + secondTotal[9] + thirdTotal[9],
+  },
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 35,
+    season: 3,
+    affix: schedule[10],
+    expansion: "sl",
+    total: firstTotal[10] + secondTotal[10] + thirdTotal[10],
+  },
+
+  {
+    date: "2022-03-11T13:55:52PST",
+    week: 36,
+    season: 3,
+    affix: schedule[11],
+    expansion: "sl",
+    total: firstTotal[11] + secondTotal[11] + thirdTotal[11],
   },
 ];
