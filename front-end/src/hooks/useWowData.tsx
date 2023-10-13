9cc718a961433dfa4ce9f59220892281d5ab3b12
Bernard Yang <ybernard@gmail.com>
2023-10-12 23:12:18 -0700

    comment out aws server call

--- front-end/src/hooks/useWowData.tsx
+++ front-end/src/hooks/useWowData.tsx
@@ -22,55 +22,55 @@ const useWowData = (expansionName: string, season: number) => {
   const [data, setData] = useState<IProps[]>([]);
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch(process.env.REACT_APP_API_URL!, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (!response.ok) {
-          throw response;
+      // try {
+      //   const response = await fetch(process.env.REACT_APP_API_URL!, {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   });
+      //   if (!response.ok) {
+      //     throw response;
+      //   }
+      // const responseData = await response.json();
+      const responseData = mockData;
+
+      // filter array for the current expansion then current season and finally sort by the week
+      const filterExpansionSeason = responseData
+        .filter((item: IProps) => {
+          return item.expansion === expansionName;
+        })
+        .filter((item: IProps) => {
+          return item.season === season;
+        })
+        .sort((a: IProps, b: IProps) => a.week - b.week);
+
+      // helper function to find week in findSchedule
+      const filterWeekByIndex = (week: number) => {
+        return filterExpansionSeason.findIndex(
+          (obj: IProps) => obj.week === week
+        );
+      };
+
+      // map new key value onto a new array
+      const findSchedule = filterExpansionSeason.map((obj: IProps) => {
+        if (obj.week >= 11) {
+          return {
+            ...obj,
+            total:
+              filterExpansionSeason[filterWeekByIndex(obj.week)].total -
+              filterExpansionSeason[filterWeekByIndex(obj.week - 10)].total,
+          };
+        } else {
+          return { ...obj };
         }
-        // const responseData = await response.json();
-        const responseData = mockData;
+      });
 
-        // filter array for the current expansion then current season and finally sort by the week
-        const filterExpansionSeason = responseData
-          .filter((item: IProps) => {
-            return item.expansion === expansionName;
-          })
-          .filter((item: IProps) => {
-            return item.season === season;
-          })
-          .sort((a: IProps, b: IProps) => a.week - b.week);
-
-        // helper function to find week in findSchedule
-        const filterWeekByIndex = (week: number) => {
-          return filterExpansionSeason.findIndex(
-            (obj: IProps) => obj.week === week
-          );
-        };
-
-        // map new key value onto a new array
-        const findSchedule = filterExpansionSeason.map((obj: IProps) => {
-          if (obj.week >= 11) {
-            return {
-              ...obj,
-              total:
-                filterExpansionSeason[filterWeekByIndex(obj.week)].total -
-                filterExpansionSeason[filterWeekByIndex(obj.week - 10)].total,
-            };
-          } else {
-            return { ...obj };
-          }
-        });
-
-        setData(findSchedule);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
+      setData(findSchedule);
+      setLoading(false);
+      // } catch (error) {
+      //   setError(error.message);
+      //   setLoading(false);
+      // }
     };
 
     fetchData();
